ed9110aecca0669e346fc14f009dbe962f6288a8
kujquality <kujquality@gmail.com>
2023-10-03 16:52:17 +0700

    first commit

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var password = "Quality";
 function passcheck() {
   if (document.getElementById("pass1").value != password) {
     alert("Wrong Password, Try Again.");
-    return;
+    return false;
   }
 }
